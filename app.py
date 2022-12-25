8fbae2cc5055e2ecd08df04562ad91f1cca37562
ggrass1585 <71492770+ggrass1585@users.noreply.github.com>
2022-12-25 15:47:07 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import gradio as gr
+from fastai.vision.all import *
+import skimage
+
+learn = load_learner('export.pkl')
+
+labels = learn.dls.vocab
+
+def predict(img):
+    img = PILImage.create(img)
+    pred,idx,probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+title = 'Bird Classifier'
+description = 'Classify the species of your bird'
+
+iface = gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(512,512)), outputs=gr.outputs.Label(num_top_classes=3), title=title, description=description)
+iface.launch()
